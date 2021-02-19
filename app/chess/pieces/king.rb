33b973aa711a36b1d599102e3a9192925853c442
IgorBanjac91 <igor.banjac91@gmail.com>
2021-02-19 11:15:22 +0100

    refactor King

--- app/chess/pieces/king.rb
+++ app/chess/pieces/king.rb
@@ -12,13 +12,5 @@ module Chess
       arr += orizontal_moves(board, [], 1)
       arr += vertical_moves(board, [], 1)
     end
-
-    def king_moves(board, arr = [])
-      arr + diagonal_moves(board, [], 1)
-      arr + orizontal_moves(board, [], 1)
-      arr + vertical_moves(board, [], 1)
-    end
-
-    # try to make a vertical_make to make a single move by default
   end
 end
